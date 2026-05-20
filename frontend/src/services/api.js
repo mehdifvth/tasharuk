13cafa5ecfe7fe5b3dde64563bb2fcc4a5e70dad
mehdi.fvth <mehdi.fathallah02@gmail.com>
2026-05-20 14:14:57 +0100

    fix VITE_API_URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
   headers: {
     'Accept': 'application/json'
   }
